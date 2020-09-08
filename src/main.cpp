ba69d01ac80b4f184252cd68970668ee787478c4
Sille Van Landschoot <info@sillevl.be>
2020-09-08 15:01:20 +0200

    refactor to single file example

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -19,13 +19,24 @@ limitations under the License.
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
 
-#include "main_functions.h"
-#include "constants.h"
 #include "model.h"
-#include "output_handler.h"
 
 #include "mbed.h"
 #include "stdlib.h"
+
+
+// This constant represents the range of x values our model was trained on,
+// which is from 0 to (2 * Pi). We approximate Pi to avoid requiring additional
+// libraries.
+const float kXrange = 2.f * 3.14159265359f;
+
+// This constant determines the number of inferences to perform across the range
+// of x values defined above. Since each inference takes time, the higher this
+// number, the more time it will take to run through the entire range. The value
+// of this constant can be tuned so that one full cycle takes a desired amount
+// of time. Since different devices take different amounts of time to perform
+// inference, this value should be defined per-device.
+const int kInferencesPerCycle = 100;
 
 PwmOut led(LED1);
 
@@ -116,12 +127,24 @@ void loop() {
   float y_val = output->data.f[0];
   led = abs(y_val);
 
-  // Output the results. A custom HandleOutput function can be implemented
-  // for each supported hardware target.
-  HandleOutput(error_reporter, x_val, y_val);
+  // Output the results.
+  // Log the current X and Y values
+  TF_LITE_REPORT_ERROR(error_reporter, "x_value: %f, y_value: %f\n",
+                       static_cast<double>(x_val),
+                       static_cast<double>(y_val));
 
   // Increment the inference_counter, and reset it if we have reached
   // the total number per cycle
   inference_count += 1;
   if (inference_count >= kInferencesPerCycle) inference_count = 0;
 }
+
+
+int main(void) {
+  setup();
+  while (true) {
+    loop();
+  }
+}
+
+
